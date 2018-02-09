1b8a3dfa41e5104da0db8798d241849874b3ed16
ojaswin <ojaswin@hotmail.com>
2018-02-09 16:23:01 +0530

    Fixed critical issues

--- interaction.py
+++ interaction.py
@@ -1,2 +1,18 @@
-# -*- coding: utf-8 -*-
+from board import *
+from player import *
 
+br = Board()
+br.initialise()
+
+p = Player(br)
+
+p.moveDown()
+p.moveRight()
+p.moveDown()
+p.moveLeft()
+p.moveUp()
+
+br.playerPosC = p.currX
+br.playerPosR = p.currY
+
+br.display()
--- player.py
+++ player.py
@@ -1,23 +1,25 @@
-class Player:
-    def __init__(self, board, x = 1, y = 1):
+from board import *
+
+class Player(Board):
+    def __init__(self, brd, x = 1, y = 1):
         self.currX = x
         self.currY= y
         self.moves = []
+        self.board = brd.state
 
-    def moveUp(self):
-        if board[self.currX - 1][self.currY] != '#':
+    def moveLeft(self):
+        if self.board[self.currX - 1][self.currY] != '#':
             self.currX -= 1
             self.moves.append((self.currX, self.currY))
-    def moveDown(self):
-        if board[self.currX + 1][self.currY] != '#':
+    def moveRight(self):
+        if self.board[self.currX + 1][self.currY] != '#':
             self.currX += 1
             self.moves.append((self.currX, self.currY))
-    def moveLeft(self):
-        if board[self.currX][self.currY - 1] != '#':
+    def moveUp(self):
+        if self.board[self.currX][self.currY - 1] != '#':
             self.currY -= 1
             self.moves.append((self.currX, self.currY))
-    def moveRight(self):
-        if board[self.currX][self.currY + 1] != '#':
+    def moveDown(self):
+        if self.board[self.currX][self.currY + 1] != '#':
             self.currY += 1
             self.moves.append((self.currX, self.currY))
-
