f27df17c641b0ed36a8fa9a0043617ff294ff149
Feirydraco <majinsparks@gmail.com>
2018-02-11 16:00:35 +0530

    added intial q matrix

--- train.py
+++ train.py
@@ -3,9 +3,15 @@ from player import Player
 import numpy as np
 
 def initRewards(R,row,col):
-    R[row-1,col-1] = 100
+    R[row,col] = 100
 
 br = Board()
-R = np.zeros((br.h,br.w))
-initRewards(R,br.goalR,br.goalC)
-print R
+R = np.zeros((br.h + 2,br.w + 2))
+
+def encloseMinusOne(R):
+    for i in range(R.shape[0]):
+        for j in range(R.shape[1]):
+            if i == 0 or i == R.shape[0] - 1:
+                R[i, j] = -1
+            if j == 0 or j == R.shape[1] - 1:
+                R[i, j] = -1
