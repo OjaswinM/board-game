3d48c26ddd50f32eccee315a516c25596f1df94b
ojaswin <ojaswin@hotmail.com>
2018-02-12 19:46:31 +0530

    Updated train.py

--- train.py
+++ train.py
@@ -2,11 +2,10 @@ from board import Board
 from player import Player
 import numpy as np
 
-def initRewards(R,row,col):
-    R[row,col] = 100
+gammaFactor = 0.8
 
-br = Board()
-R = np.zeros((br.h + 2,br.w + 2))
+def initRewards(R,row,col):
+    R[row,col] = 100        #set reward cell as 100 in R
 
 def encloseMinusOne(R):
     for i in range(R.shape[0]):
@@ -14,4 +13,10 @@ def encloseMinusOne(R):
             if i == 0 or i == R.shape[0] - 1:
                 R[i, j] = -1
             if j == 0 or j == R.shape[1] - 1:
-                R[i, j] = -1
+                R[i, j] = -1        #enclose numpy array with -1
+
+def getQ(R,Q,row,col):          #get the new value of Q
+    return (R[row,col] + gammaFactor*(findMax(Q,row,col)))
+
+def findMax(Q,row,col):
+    return max(Q[row+1,col],Q[row-1,col],Q[row,col-1],Q[row,col+1])
